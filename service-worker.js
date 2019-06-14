10ad92d4bb7421a413d46f8b9117c3d6b60a5252
mkono614 <mkono.bp@gmail.com>
2019-06-14 14:00:32 +0900

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-});
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
 
-// Œ»ó‚Å‚ÍA‚±‚Ìˆ—‚ğ‘‚©‚È‚¢‚ÆService Worker‚ª—LŒø‚Æ”»’è‚³‚ê‚È‚¢‚æ‚¤‚Å‚·
 self.addEventListener("push", function(event) {
   event.waitUntil(
     self.registration.pushManager.getSubscription()
