246d41c4a511de2f540c8dfe43efe6148c309981
mkono614 <mkono.bp@gmail.com>
2019-06-21 14:01:44 +0900

    サンプル変更

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  document.getElementById('fahrenheitInput').addEventListener('input', function(event) {
+    celsiusInput.value=((this.value - 32) / 1.8).toFixed(2);
+  });
+
+  document.getElementById('celsiusInput').addEventListener('input', function(event) {
+    fahrenheitInput.value=((this.value * 1.8) + 32).toFixed(2);
+  });
+
+})();
