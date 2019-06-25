c44db1d3481723ac59b92480b672f8c1357486ca
mkono614 <mkono.bp@gmail.com>
2019-06-25 14:55:17 +0900

    さらにさらにテスト

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,17 +1,3 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
-
-
-
 importScripts('https://www.gstatic.com/firebasejs/5.5.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.4/firebase-messaging.js');
 
